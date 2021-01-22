af6f72736a804a740f3338eb6e2cd36172f89076
Alexey Igrychev <alexey.igrychev@flant.com>
2021-01-22 14:35:27 +0000

    test

--- integration/suites/giterminism/config_test.go
+++ integration/suites/giterminism/config_test.go
@@ -1,10 +1,10 @@
 package giterminism_test
 
 import (
+	"bytes"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/ginkgo/extensions/table"
 	. "github.com/onsi/gomega"
-
 	"github.com/werf/werf/integration/pkg/utils"
 )
 
@@ -15,83 +15,173 @@ var _ = Describe("config", func() {
 		gitAddAndCommit("werf-giterminism.yaml")
 	})
 
-	type entry struct {
-		allowUncommitted        bool
-		addConfig               bool
-		commitConfig            bool
-		changeConfigAfterCommit bool
-		expectedErrSubstring    string
-	}
+	{
+		type entry struct {
+			allowUncommitted        bool
+			addConfig               bool
+			commitConfig            bool
+			changeConfigAfterCommit bool
+			expectedErrSubstring    string
+		}
 
-	DescribeTable("config.allowUncommitted",
-		func(e entry) {
-			var contentToAppend string
-			if e.allowUncommitted {
-				contentToAppend = `
+		DescribeTable("config.allowUncommitted",
+			func(e entry) {
+				var contentToAppend string
+				if e.allowUncommitted {
+					contentToAppend = `
 config:
   allowUncommitted: true`
-			} else {
-				contentToAppend = `
+				} else {
+					contentToAppend = `
 config:
   allowUncommitted: false`
-			}
-			fileCreateOrAppend("werf-giterminism.yaml", contentToAppend)
-			gitAddAndCommit("werf-giterminism.yaml")
+				}
+				fileCreateOrAppend("werf-giterminism.yaml", contentToAppend)
+				gitAddAndCommit("werf-giterminism.yaml")
 
-			if e.addConfig {
-				fileCreateOrAppend("werf.yaml", `
+				if e.addConfig {
+					fileCreateOrAppend("werf.yaml", `
 configVersion: 1
 project: none
 ---
 `)
-			}
+				}
 
-			if e.commitConfig {
-				gitAddAndCommit("werf.yaml")
-			}
+				if e.commitConfig {
+					gitAddAndCommit("werf.yaml")
+				}
 
-			if e.changeConfigAfterCommit {
-				fileCreateOrAppend("werf.yaml", "\n")
-			}
+				if e.changeConfigAfterCommit {
+					fileCreateOrAppend("werf.yaml", "\n")
+				}
 
-			output, err := utils.RunCommand(
+				output, err := utils.RunCommand(
+					SuiteData.TestDirPath,
+					SuiteData.WerfBinPath,
+					"config", "render",
+				)
+
+				if e.expectedErrSubstring != "" {
+					Ω(err).Should(HaveOccurred())
+					Ω(string(output)).Should(ContainSubstring(e.expectedErrSubstring))
+				} else {
+					Ω(err).ShouldNot(HaveOccurred())
+				}
+			},
+			Entry("werf.yaml not found", entry{
+				expectedErrSubstring: `unable to read werf config: the file 'werf.yaml' not found in the project git repository`,
+			}),
+			Entry("werf.yaml not committed", entry{
+				addConfig:            true,
+				expectedErrSubstring: `unable to read werf config: the file 'werf.yaml' must be committed`,
+			}),
+			Entry("werf.yaml committed", entry{
+				addConfig:    true,
+				commitConfig: true,
+			}),
+			Entry("werf.yaml committed, werf.yaml has uncommitted changes", entry{
+				addConfig:               true,
+				commitConfig:            true,
+				changeConfigAfterCommit: true,
+				expectedErrSubstring:    `unable to read werf config: the file 'werf.yaml' changes must be committed`,
+			}),
+			Entry("config.allowUncommitted is true, werf.yaml not committed", entry{
+				allowUncommitted: true,
+				addConfig:        true,
+			}),
+			Entry("config.allowUncommitted is true, werf.yaml committed", entry{
+				allowUncommitted: true,
+				addConfig:        true,
+				commitConfig:     true,
+			}),
+		)
+	}
+
+	{
+		type entry struct {
+			allowUncommitted     bool
+			addSymlinks          bool
+			commitSymlinks       bool
+			expectedErrSubstring string
+		}
+
+		createSymlinkFile := func(relPath string, link string) {
+			hashBytes, _ := utils.RunCommandWithOptions(
 				SuiteData.TestDirPath,
-				SuiteData.WerfBinPath,
-				"config", "render",
+				"git",
+				[]string{"hash-object", "-w", "--stdin"},
+				utils.RunCommandOptions{
+					ToStdin:       link,
+					ShouldSucceed: true,
+				},
 			)
 
-			if e.expectedErrSubstring != "" {
-				Ω(err).Should(HaveOccurred())
-				Ω(string(output)).Should(ContainSubstring(e.expectedErrSubstring))
-			} else {
-				Ω(err).ShouldNot(HaveOccurred())
-			}
-		},
-		Entry("werf.yaml not found", entry{
-			expectedErrSubstring: `unable to read werf config: the file 'werf.yaml' not found in the project git repository`,
-		}),
-		Entry("werf.yaml not committed", entry{
-			addConfig:            true,
-			expectedErrSubstring: `unable to read werf config: the file 'werf.yaml' must be committed`,
-		}),
-		Entry("werf.yaml committed", entry{
-			addConfig:    true,
-			commitConfig: true,
-		}),
-		Entry("werf.yaml committed, werf.yaml has uncommitted changes", entry{
-			addConfig:               true,
-			commitConfig:            true,
-			changeConfigAfterCommit: true,
-			expectedErrSubstring:    `unable to read werf config: the file 'werf.yaml' changes must be committed`,
-		}),
-		Entry("config.allowUncommitted is true, werf.yaml not committed", entry{
-			allowUncommitted: true,
-			addConfig:        true,
-		}),
-		Entry("config.allowUncommitted is true, werf.yaml committed", entry{
-			allowUncommitted: true,
-			addConfig:        true,
-			commitConfig:     true,
-		}),
-	)
+			utils.RunSucceedCommand(
+				SuiteData.TestDirPath,
+				"git",
+				"update-index", "--add", "--cacheinfo", "120000", string(bytes.TrimSpace(hashBytes)), relPath,
+			)
+
+			utils.RunSucceedCommand(
+				SuiteData.TestDirPath,
+				"git",
+				"checkout", relPath,
+			)
+		}
+
+		DescribeTable("config.allowUncommitted xxxxxxxxxxxxxx",
+			func(e entry) {
+				var contentToAppend string
+				if e.allowUncommitted {
+					contentToAppend = `
+config:
+  allowUncommitted: true`
+				} else {
+					contentToAppend = `
+config:
+  allowUncommitted: false`
+				}
+				fileCreateOrAppend("werf-giterminism.yaml", contentToAppend)
+				gitAddAndCommit("werf-giterminism.yaml")
+
+				if e.addSymlinks {
+					fileCreateOrAppend("test", `
+configVersion: 1
+project: none
+---
+`)
+					createSymlinkFile("werf.yaml", "test")
+				}
+
+				if e.commitSymlinks {
+					utils.RunSucceedCommand(
+						SuiteData.TestDirPath,
+						"git",
+						"commit", "-m", "Commit symlinks",
+					)
+				}
+
+				output, err := utils.RunCommand(
+					SuiteData.TestDirPath,
+					SuiteData.WerfBinPath,
+					"config", "render",
+				)
+
+				if e.expectedErrSubstring != "" {
+					Ω(err).Should(HaveOccurred())
+					Ω(string(output)).Should(ContainSubstring(e.expectedErrSubstring))
+				} else {
+					Ω(err).ShouldNot(HaveOccurred())
+				}
+			},
+			Entry("werf.yaml not committed", entry{
+				addSymlinks:          true,
+				expectedErrSubstring: `unable to read werf config: the file 'werf.yaml' must be committed`,
+			}),
+			Entry("werf.yaml committed", entry{
+				addSymlinks:    true,
+				commitSymlinks: true,
+			}),
+		)
+	}
 })
