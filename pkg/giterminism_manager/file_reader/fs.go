cf2b70fdf1a241046c46d1bcf8bc0489844a6b79
Alexey Igrychev <alexey.igrychev@flant.com>
2021-01-25 17:35:42 +0000

    +

--- pkg/giterminism_manager/file_reader/fs.go
+++ pkg/giterminism_manager/file_reader/fs.go
@@ -227,39 +227,40 @@ func (r FileReader) ResolveFilePath(relPath string) (string, error) {
 }
 
 func (r FileReader) resolveFilePath(relPath string, depth int, checkFunc func(resolvedPath string) error) (string, error) {
-	fmt.Println(relPath, depth)
-
 	if depth > 1000 {
 		return "", TooManyLevelsOfSymbolicLinksErr
 	}
 	depth++
 
 	// returns path if the corresponding file is File or Directory.
-	//{
-	//	absPath := filepath.Join(r.sharedContext.ProjectDir(), relPath)
-	//	stat, err := os.Stat(absPath)
-	//	if err != nil {
-	//		if os.IsNotExist(err) || util.IsNotADirectoryError(err) {
-	//			goto afterBlock
-	//		}
-	//
-	//		return "", fmt.Errorf("unable to access file %s: %s", absPath, err)
-	//	}
-	//
-	//	if stat.Mode()&os.ModeSymlink == os.ModeSymlink {
-	//		goto afterBlock
-	//	}
-	//
-	//	if checkFunc != nil {
-	//		if err := checkFunc(relPath); err != nil {
-	//			return "", err
-	//		}
-	//	}
-	//
-	//	return relPath, nil
-	//}
+	{
+		absPath := filepath.Join(r.sharedContext.ProjectDir(), relPath)
+		stat, err := os.Stat(absPath)
+		if err != nil {
+			if os.IsNotExist(err) || util.IsNotADirectoryError(err) {
+				goto afterBlock
+			}
 
-	//afterBlock:
+			return "", fmt.Errorf("unable to access file %s: %s", absPath, err)
+		}
+
+		if stat.Mode()&os.ModeSymlink == os.ModeSymlink {
+			fmt.Println("!!!!!!!!!1")
+			goto afterBlock
+		} else {
+			fmt.Println("!!!!!!!!!2")
+		}
+
+		if checkFunc != nil {
+			if err := checkFunc(relPath); err != nil {
+				return "", err
+			}
+		}
+
+		return relPath, nil
+	}
+
+afterBlock:
 
 	pathParts := util.SplitFilepath(relPath)
 	pathPartsLen := len(pathParts)
